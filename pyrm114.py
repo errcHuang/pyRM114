7949ad4789df41f2c0af615107f8b346a7209f1a
Eric Huang <eric.huanghg@gmail.com>
2016-07-06 11:41:03 -0400

    fixed

--- pyrm114.py
+++ pyrm114.py
@@ -1,20 +1,18 @@
 from tweepy import OAuthHandler
 from sklearn.metrics import *
+from collections import namedtuple
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sys
 import subprocess
 import argparse
-import tweepy
 import random
-import re
 try:
     import cPickle as pickle
 except:
     import pickle
 
-__directory__ = '' #placeholder for directory
 def main(argv):
     #Create top level parser
     parser = argparse.ArgumentParser(
@@ -60,33 +58,32 @@ def main(argv):
 
     #parse the args and call whichever function was selected (func=...)
     args = parser.parse_args()
-    args.func(args)
+    #args.func(args)
 
 #basically training and test set partitioning is outside scope of class,
 #though there will be an evaluator function
-class pyRM114:
+class pyrm114:
     PROB_LIST = namedtuple('PROB_LIST', ['match', 'probability', 'pr'])
 
-    def __init__(self, *args, directory='.', algorithm='osb unique microgroom'):
-        self.categories = args #list of categories to classify to
+    def __init__(self, list_of_categories, directory=os.getcwd(), algorithm='osb unique microgroom'):
+        self.categories = list_of_categories #list of categories to classify to
         self.directory = directory #directory to save all files
         self.algorithm = algorithm
-        _create_crm_files(categories, algorithm)
+        self._create_crm_files(self.categories, algorithm)
 
-        #its user responsibility to separate training files into small and separate textfiles
     
     def train(self, category, training_string):
         #write to text file
         file_dir = os.path.join(self.directory, 'train.tmp')
         with open(file_dir, 'w') as f:
             f.write(training_string)
-        _train(file_dir, category)    
+        self._train(file_dir, category)    
         subprocess.Popen(['rm', '-rf',file_dir])
 
     #by default will split training files into lines and train each line
-    def train_textfile(self, category, *args, train_method='TET', delimiter='\n'):
+    def train_textfile(self, category, train_method='TET', delimiter='\n', *args):
         #check if category exists
-        if category is not in self.categories:
+        if category not in self.categories:
             raise IndexError('Category does not exist!')
             return
 
@@ -105,8 +102,8 @@ class pyRM114:
                     with open('train.tmp', 'w') as t:
                         t.write(section)
                     #attempt to classify and then train based on results
-                    best_match, prob_list = _classify('train.tmp')
-                    _smart_train(best_match, prob_list, category, train_method, 'train.tmp')
+                    best_match, prob_list = self._classify('train.tmp')
+                    self._smart_train(best_match, prob_list, category, train_method, 'train.tmp')
                     #delete temporary file
                     subprocess.Popen('rm -rf train.tmp', shell=True)
 
@@ -115,16 +112,18 @@ class pyRM114:
         file_dir = os.path.join(self.directory, 'classify.tmp')
         with open(file_dir, 'w') as f:
             f.write(string)
-        bestMatch, probList = _classify(file_dir)
-            print 'best match: ' + bestMatch
-            print 'probabilities:'
-            for tup in probList:
-                print '\t' + str(tup[0]) + ': ' + str(tup[1])
+        bestMatch, probList = self._classify(file_dir)
+        print 'best match: ' + bestMatch
+        print 'probabilities:'
+        for tup in probList:
+            print '\t' + str(tup[0]) + ': ' + str(tup[1]) #prints probability and pR
+
+        subprocess.Popen(['rm', file_dir])
     
     #takes filename as args
     def classify_textfiles(self, *args):
         for textfile in args:
-            bestMatch, probList = _classify(str(textfile))
+            bestMatch, probList = self._classify(str(textfile))
             print 'best match: ' + bestMatch
             print 'probabilities:'
             for tup in probList:
@@ -132,73 +131,72 @@ class pyRM114:
             print
         #clean_workspace()
 
-    def reset(corpus=True, crm=True):
+    def reset(self, corpus=True, crm=True):
         if (corpus is True):
             subprocess.call('rm -f *.css', shell=True) #remove all corpus type files
 
         if (crm is True):
             subprocess.call('rm -f *.crm', shell=True)
 
-    def evaluate(self):
+    def evaluate(self, y_true, y_pred, output=sys.stdout):
         #if --eval flag is used, then classify test set and print statistics
-        if (args.eval is not None):
-            print 'evaluating algorithm...'
+        print 'evaluating algorithm...'
+
+        temp_stats_file = open(os.path.join(self.directory, 'prob_distribution.txt'), 'w')
+
+        print >>output,'------ EVALUATION STATS ------'
+        #Compute Accuracy Score
+        print >>output, 'Accuracy score (normalized):', accuracy_score(y_true, y_pred)
+        print >>output
+
+        #Confusion Matrix
+        cm = confusion_matrix(y_true, y_pred, labels=self.categories)
+        print >>output, 'Confusion matrix:\n', cm
+        print >>output
+
+        #Classification report
+        print >>output, classification_report(y_true, y_pred, target_names=self.categories)
+        output.close()
+
+        #show confusion matrix
+        plt.figure()
+        self._plot_confusion_matrix(cm, self.categories)
+        plt.show()
+     
+
+    def crm_files_exist(self):
+        #check if crm files exist already
+        allFilesExist = True
+        for category in self.categories:
+            complete_directory = os.path.join(self.directory, category + '.css')
+            if(os.path.isfile(complete_directory) is False):
+                allFilesExist = False
+                break
+        return allFilesExist
+
+   
+    def _plot_confusion_matrix(self, cm, labels, title='Confusion matrix', cmap=plt.cm.Blues):
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(labels))
+        plt.xticks(tick_marks, labels, rotation=45)
+        plt.yticks(tick_marks, labels)
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
 
 
-            temp_stats_file = open(os.path.join(self.directory, 'prob_distribution.txt'), 'w')
-
-            y_true = []
-            y_pred = []
-            print >>temp_stats_file,'------ BEST MATCH AND PROBABILITIES ------'
-
-
-                for t in tweets:
-                    trueAuthor = t.author.screen_name
-                    tweetFileName = write_tweets_to_file([t], self.directory, trueAuthor + '.txt')
-                    bestMatch, probList = _classify(tweetFileName)
-
-                    #Write best match and probabilities either to std.out or to file
-                    print >>temp_stats_file, 'best match: ' + bestMatch[0]
-                    print >>temp_stats_file, 'probabilities:'
-                    for tup in probList:
-                        print >>temp_stats_file, '\t' + str(tup[0]) + ': ' + str(tup[1])
-                    print >>temp_stats_file, ''
-
-                    y_true.append(trueAuthor)
-                    y_pred.append(bestMatch[0])
-            temp_stats_file.close()
-
-            print >>args.eval,'------ EVALUATION STATS ------'
-            #Compute Accuracy Score
-            print >>args.eval, 'Accuracy score (normalized):', accuracy_score(y_true, y_pred)
-            print >>args.eval
-
-            #Confusion Matrix
-            cm = confusion_matrix(y_true, y_pred, labels=screen_names)
-            print >>args.eval, 'Confusion matrix:\n', cm
-            print >>args.eval
-
-            #Classification report
-            print >>args.eval, classification_report(y_true, y_pred, target_names=screen_names)
-            args.eval.close()
-
-            #show confusion matrix
-            plt.figure()
-            plot_confusion_matrix(cm, screen_names)
-            plt.show()
-
-        clean_workspace(screen_names)
-    
     #match: (name_of_match, prob_of_match, pr_of_match)
     #probList = [ (name1, probability1, pR1) (name2, probability2, pR2) ...]
     #train_method: (name_of_method, pR_threshold)
-    def _smart_train(match, probList, truth_match_name, train_method, textfilename,
+    def _smart_train(self, match, probList, truth_match_name, train_method, textfilename,
             prThreshold = 10.0):
         name_of_match = match[0]
         pr = match[2]
         if train_method == 'TOE': #train on error
             if truth_match_name != name_of_match: #if classifier incorrectly predicts 
-                _train(textfilename, truth_match_name)
+                self._train(textfilename, truth_match_name)
             else:
                 #make sure that all the probabilities are different
                 #else it's basically the same as random guessing
@@ -211,17 +209,17 @@ class pyRM114:
                     previousProb = probTuple[2]
                 
                 if isEqual is True:
-                    _train(textfilename, truth_match_name)
+                    self._train(textfilename, truth_match_name)
         elif train_method == 'SSTTT':
             if truth_match_name != name_of_match: #if classifier incorrectly predicts 
-                _train(textfilename, truth_match_name)
+                self._train(textfilename, truth_match_name)
             elif pr < prThreshold: #if correct, but match PR is less than PR threshold
-                _train(textfilename, truth_match_name)
+                self._train(textfilename, truth_match_name)
         elif train_method == 'DSTTT':
             if truth_match_name != name_of_match: #if classifier incorrectly predicts 
-                _train(textfilename, truth_match_name)
+                self._train(textfilename, truth_match_name)
             elif pr < prThreshold: #if correct, but match PR is less than PR threshold
-                _train(textfilename, truth_match_name)
+                self._train(textfilename, truth_match_name)
 
                 #if not sure if others were incorrect, then untrain out 
                 for tuples in probList:
@@ -229,13 +227,13 @@ class pyRM114:
                     if name != truth_match_name:
                         prWrong = abs(tuples[2])
                         if prWrong < prThreshold:
-                            _untrain(textfilename, name)
+                            self._untrain(textfilename, name)
         elif train_method == 'DSTTTR':
             oldPR = match[2]
             if truth_match_name != name_of_match: #if classifier incorrectly predicts 
-                _train(textfilename, truth_match_name)
+                self._train(textfilename, truth_match_name)
             elif pr < prThreshold: #if correctly matched, but match PR is less than PR threshold
-                _train(textfilename, truth_match_name)
+                self._train(textfilename, truth_match_name)
 
                 #reclassify text
                 bestMatch, probList = _classify(textfilename)
@@ -247,10 +245,10 @@ class pyRM114:
                     for tuples in probList:
                         name = tuples[0]
                         if name != truth_match_name:
-                            _untrain(textfilename, name)
+                            self._untrain(textfilename, name)
         elif train_method == 'TTE':
             if truth_match_name != name_of_match: #if classifier incorrectly predicts 
-                _train(textfilename, truth_match_name)
+                self._train(textfilename, truth_match_name)
 
                 #reclassify text
                 bestMatch, probList = _classify(textfilename)
@@ -260,7 +258,7 @@ class pyRM114:
                 #keep training until pr threshold improves to satisfactory level
                 loopCount = 0
                 while (newPR < prThreshold and loopCount is not 5):
-                    _train(textfilename, truth_match_name)
+                    self._train(textfilename, truth_match_name)
 
                     #reclassify text
                     bestMatch, probList = _classify(textfilename)
@@ -269,22 +267,24 @@ class pyRM114:
         elif train_method == 'TUNE':
             raise Exception('TUNE not implemented yet')
         else: #by default train everything (TET)
-            _train(textfilename, truth_match_name)
+            self._train(textfilename, truth_match_name)
 
 
 
-    def _train(trainingTxtFile, corpus_name):
-        subprocess.call('crm ' + os.path.join(__directory__, 'learn.crm') +
+    def _train(self, trainingTxtFile, corpus_name):
+        if corpus_name not in self.categories:
+            raise ValueError('Category doesn\'t exist!')
+        subprocess.call('crm ' + os.path.join(self.directory, 'learn.crm') +
                 ' ' + (corpus_name+'.css') + ' < '+ trainingTxtFile, shell=True)
-    def _untrain(trainingTxtFile, corpus_name):
-        subprocess.call('crm ' + os.path.join(__directory__, 'unlearn.crm') +
+    def _untrain(self, trainingTxtFile, corpus_name):
+        subprocess.call('crm ' + os.path.join(self.directory, 'unlearn.crm') +
                 ' ' + (corpus_name+'.css') + ' < '+ trainingTxtFile, shell=True)
 
     # classifies textfile and returns best match and probabilities
     # bestMatch = tuple(bestMatch bestProb, bestPR)
     # probList = [ (twitterHandle1, probability1, pR1) (twitterHandle2, probability2, pR2) ...]
-    def _classify(textFileName):
-        output =  subprocess.check_output('crm ' + os.path.join(__directory__, 'classify.crm') + ' < ' + textFileName, shell=True) #string output from crm114
+    def _classify(self, textFileName):
+        output =  subprocess.check_output('crm ' + os.path.join(self.directory, 'classify.crm') + ' < ' + textFileName, shell=True) #string output from crm114
         outList = output.split()
         bestMatch = ( str(outList[0]), float(outList[1]), float(outList[2]) ) #(match, prob, pR)
         outList = outList[3:]
@@ -293,7 +293,7 @@ class pyRM114:
         it = iter(outList)
         for x in it:
             x.rstrip(':')
-            p = PROB_LIST(x, float(next(it)), float(next(it)))
+            p = (x, float(next(it)), float(next(it)))
             probList.append(p)
             #probList.append((x, float(next(it)), float(next(it)) ))
         
@@ -301,7 +301,7 @@ class pyRM114:
 
         return (bestMatch, tuple(probList)) 
 
-    def _create_crm_files(file_names, classification_type):
+    def _create_crm_files(self, file_names, classification_type):
         #classification_type = classification_type.rstrip('>').strip('<')
         CLASSIFY_EXT = '.css'    #create files if they don't exist
         UNLEARN_CMD = "{ learn <%s refute> (:*:_arg2:) }"
@@ -318,14 +318,16 @@ class pyRM114:
                     ' match [:%s_temp:] (:: :%s_prob: :%s_pr:)' \
                     ' /prob: ([[:graph:]]+), pR:[[:space:]]+([[:graph:]]+)/;'
         #create learn.crm
-        learnCRM = open(os.path.join(__directory__,'learn.crm'), 'w')
+        learnCRM = open(os.path.join(self.directory,'learn.crm'), 'w')
         learnCRM.write(LEARN_CMD % classification_type)
         learnCRM.close()
+        #print 'created learn.crm'
 
         #create unlearn.crm
-        unlearnCRM = open(os.path.join(__directory__, 'unlearn.crm'), 'w')
+        unlearnCRM = open(os.path.join(self.directory, 'unlearn.crm'), 'w')
         unlearnCRM.write(UNLEARN_CMD % classification_type)
         unlearnCRM.close()
+        #print 'created unlearn.crm'
 
         #create classify.crm
         classifyCRM = open('classify.crm', 'w')
@@ -339,35 +341,22 @@ class pyRM114:
                                           ' '.join(output_list)
                                          ))
         classifyCRM.close()
+        #print 'created classify.crm'
 
         CRM_BINARY = 'crm'
         CLASSIFICATION_EXT = '.css'
 
         #create corpus files
         for n in file_names:
-            pipe = os.popen(('crm '+ os.path.join(__directory__) + 'learn.crm ' +  str(n + CLASSIFICATION_EXT)), 'w')
+            pipe = os.popen(('crm '+ os.path.join(self.directory,'learn.crm') + ' ' +  str(n + CLASSIFICATION_EXT)), 'w')
             pipe.close()
+        #print 'created corpus files'
 
-
-
-
-
-
-
-def crm_files_exist(screen_names):
-    #check if files exit already
-    allFilesExist = True
-    for name in screen_names:
-        complete_directory = os.path.join(__directory__, name + '.css')
-        if(os.path.isfile(complete_directory) is False):
-            allFilesExist = False
-            break
-    return allFilesExist
 
 #slices a list into n nearly-equal-length partitions
 #returns list of lists
 def random_partition(lst, n):
-    random.shuffle(lst)
+    random.shuffle(lst) #optional
     division = len(lst) / float(n)
     return [ lst[int(round(division * i)): int(round(division * (i+1)))] for i in xrange(n) ]
 
@@ -378,33 +367,14 @@ def get_training_and_test_set(trainProportion, dataset):
     training_data = []
     test_data = []
     for data in dataset:
-        random.shuffle(data)
+        random.shuffle(data) #optional
         trainIndex = trainProportion * len(data) #calculates index for end of training set
         trainIndex = int(round(trainIndex))
         training_data.append(data[:trainIndex]) #partitions training set from start to random index
         test_data.append(data[trainIndex:]) #partition test set from random index to end
     return (training_data, test_data)
 
-
-
-
-def plot_confusion_matrix(cm, labels, title='Confusion matrix', cmap=plt.cm.Blues):
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(labels))
-    plt.xticks(tick_marks, labels, rotation=45)
-    plt.yticks(tick_marks, labels)
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-
-
-def clean_workspace(screen_names):
-    for name in screen_names:
-        subprocess.call('rm -f ' + os.path.join(__directory__, str(name)+ '.txt'), shell=True)
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
+#if __name__ == '__main__':
+#    main(sys.argv[1:])
 
 
