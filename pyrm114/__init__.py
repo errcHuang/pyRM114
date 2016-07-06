5cde69d7f0fdb8bf9cc2ae9f58248d31e8ccc026
Eric Huang <eric.huanghg@gmail.com>
2016-07-06 15:10:37 -0400

    release 0.21 with requirements.txt

---
+++ pyrm114/__init__.py
@@ -0,0 +1,5 @@
+from pyrm114 import pyrm114 
+
+__license__ = '(c) 2016 Eric Huang. ' \
+              'MIT License. See LICENSE file for details.'
+__all__ = ['pyrm114']
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyrm114',
+  packages = ['pyrm114'], # this must be the same as the name above
+  version = '0.21',
+  description = 'Python wrapper for CRM114 classifier',
+  author = 'Eric Huang',
+  author_email = 'eric.huanghg@gmail.com',
+  url = 'https://github.com/errcHuang/pyrm114', # use the URL to the github repo
+  download_url = 'https://github.com/errcHuang/pyrm114/tarball/0.1', # I'll explain this in a second
+  keywords = ['machine', 'learning', 'crm114'], # arbitrary keywords
+  classifiers = [],
+)
