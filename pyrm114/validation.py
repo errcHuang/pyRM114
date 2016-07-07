0bcb9c48ae9c0d0802cb736bd1f653751c5c2698
Eric Huang <eric.huanghg@gmail.com>
2016-07-07 15:17:33 -0400

    training and test set return dicts not list

--- pyrm114/validation.py
+++ pyrm114/validation.py
@@ -7,17 +7,18 @@ def random_partition(lst, n):
     return [ lst[int(round(division * i)): int(round(division * (i+1)))] for i in xrange(n) ]
 
 #Randomly resamples labeled datasets into comprehensive training set and test set
-#reshuffles data and returns training/test sets at
-#the list 'dataset' should have lists that represent classes
-def make_training_and_test_set(trainProportion, dataset):
+#reshuffles data and returns training/test sets as list of dictionaries
+#the arguments should be lists that represent classes
+def make_training_and_test_set(trainProportion, shuffle=True, **kwargs):
     training_data = []
     test_data = []
-    for data in dataset:
-        random.shuffle(data) #optional
-        trainIndex = trainProportion * len(data) #calculates index for end of training set
+    for key, class_list in kwargs.iteritems():
+        if shuffle is True:
+            random.shuffle(class_list) #optional
+        trainIndex = trainProportion * len(class_list) #calculates index for end of training set
         trainIndex = int(round(trainIndex))
-        training_data.append(data[:trainIndex]) #partitions training set from start to random index
-        test_data.append(data[trainIndex:]) #partition test set from random index to end
+        training_data.append( {key : class_list[:trainIndex]} ) #partitions training set from start to random index
+        test_data.append( {key : class_list[trainIndex:]} ) #partition test set from random index to end
     return (training_data, test_data)
 
 
